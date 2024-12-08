5d3ffe8847a8ed3ec051e9041957daaa9f8701ee
Robin Schroer <git@sulami.xyz>
2024-12-08 11:25:22 +0900

    Add day 5 part 2

--- src/day05.rs
+++ src/day05.rs
@@ -1,20 +1,17 @@
 use itertools::Itertools;
 use nom::{
-    character::complete::{char, u8 as parse_u8},
+    character::complete::{char, newline, u8 as parse_u8},
     combinator::all_consuming,
     multi::separated_list1,
     sequence::separated_pair,
     IResult,
 };
+use std::cmp::Ordering;
 
 super::solve!("05");
 
 fn part_1(input: &str) -> usize {
-    let rules: Vec<_> = input
-        .lines()
-        .take_while(|l| !l.is_empty())
-        .map(|l| parse_rule(l).expect("invalid rule").1)
-        .collect();
+    let rules = parse_rules(input).expect("invalid rules").1;
     input
         .lines()
         .skip(rules.len() + 1)
@@ -24,19 +21,40 @@ fn part_1(input: &str) -> usize {
         .sum()
 }
 
-fn part_2(_input: &str) -> usize {
-    0
+fn part_2(input: &str) -> usize {
+    let rules = parse_rules(input).expect("invalid rules").1;
+    input
+        .lines()
+        .skip(rules.len() + 1)
+        .map(|l| parse_manual(l).expect("invalid manual").1)
+        .filter(|m| !is_valid(m, &rules))
+        .map(|mut m| {
+            m.sort_by(|a, b| {
+                if rules.contains(&(*a, *b)) {
+                    Ordering::Less
+                } else if rules.contains(&(*b, *a)) {
+                    Ordering::Greater
+                } else {
+                    Ordering::Equal
+                }
+            });
+            m
+        })
+        .map(|m| m[m.len() / 2] as usize)
+        .sum()
 }
 
-fn parse_rule(s: &str) -> IResult<&str, (u8, u8)> {
-    all_consuming(separated_pair(parse_u8, char('|'), parse_u8))(s)
+type Rule = (u8, u8);
+
+fn parse_rules(s: &str) -> IResult<&str, Vec<Rule>> {
+    separated_list1(newline, separated_pair(parse_u8, char('|'), parse_u8))(s)
 }
 
 fn parse_manual(s: &str) -> IResult<&str, Vec<u8>> {
     all_consuming(separated_list1(char(','), parse_u8))(s)
 }
 
-fn is_valid(manual: &[u8], rules: &[(u8, u8)]) -> bool {
+fn is_valid(manual: &[u8], rules: &[Rule]) -> bool {
     rules.iter().all(|expected @ (a, b)| {
         if let Some(actual @ (_, _)) = manual
             .iter()
