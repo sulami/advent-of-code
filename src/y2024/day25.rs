f618b9d67c091479808cf1550f5d61b1af34e45f
Robin Schroer <git@sulami.xyz>
2025-01-02 18:09:31 +0900

    Cleanup 2024-25

--- src/y2024/day25.rs
+++ src/y2024/day25.rs
@@ -1,15 +1,27 @@
-use std::time::Instant;
 use crate::print_results;
+use std::time::Instant;
 
 pub fn solve() {
     let start = Instant::now();
     let input = include_str!("inputs/25");
     let keyings = parse_keyings(input);
-    let pt1 = keyings.iter().filter(|r| r.kind == Kind::Lock).map(|lock|
-        keyings.iter().filter(|r| r.kind == Kind::Key).filter(|key| {
-            lock.keying.iter().zip(key.keying).map(|(l, k)| l + k).all(|x| x <= 7)
-        }).count()
-    ).sum::<usize>();
+    let pt1 = keyings
+        .iter()
+        .filter(|r| r.kind == Kind::Lock)
+        .map(|lock| {
+            keyings
+                .iter()
+                .filter(|r| r.kind == Kind::Key)
+                .filter(|key| {
+                    lock.keying
+                        .iter()
+                        .zip(key.keying)
+                        .map(|(l, k)| l + k)
+                        .all(|x| x <= 7)
+                })
+                .count()
+        })
+        .sum::<usize>();
     print_results(2024, 25, pt1, 0, Some(start));
 }
 
@@ -17,7 +29,8 @@ type Keying = [u32; 5];
 
 #[derive(PartialEq)]
 enum Kind {
-    Lock, Key,
+    Lock,
+    Key,
 }
 
 struct Record {
@@ -26,20 +39,22 @@ struct Record {
 }
 
 fn parse_keyings(s: &str) -> Vec<Record> {
-    s.split("\n\n").map(|record| {
-        let kind = if record.starts_with("#####") {
-            Kind::Key
-        } else {
-            Kind::Lock
-        };
-        let mut keying = [0; 5];
-        record.lines().for_each(|l| {
-            l.char_indices().for_each(|(idx, c)| {
-                if c == '#' {
-                    keying[idx] += 1;
-                }
+    s.split("\n\n")
+        .map(|record| {
+            let kind = if record.starts_with("#####") {
+                Kind::Key
+            } else {
+                Kind::Lock
+            };
+            let mut keying = [0; 5];
+            record.lines().for_each(|l| {
+                l.char_indices().for_each(|(idx, c)| {
+                    if c == '#' {
+                        keying[idx] += 1;
+                    }
+                });
             });
-        });
-        Record { kind, keying }
-    }).collect()
+            Record { kind, keying }
+        })
+        .collect()
 }
